0cd19cc125da97d4d2b94728b17688f0df345f7f
Geert Fannes <geert.fannes@gmail.com>
2013-06-22 02:09:39 +0200

    One byte is sent at 4mhz

--- src/test.cpp
+++ src/test.cpp
@@ -1,20 +1,51 @@
 #include "Arduino.h"
 
-const int Pin = 4;
+const int Bit0 = 7;
+const int Bit1 = 4;
+const int Frame = 6;
+typedef unsigned char Byte;
+#if 0
+const Byte Start = 0x6f;
+const Byte Stop = 0x90;
+#define START() PORTD &= Start
+#define STOP()  PORTD |= Stop
+#else
+#define IDLE() PORTD = 0x00; PORTD = 0x00
+#define BIT0() PORTD = 0x80; PORTD = 0x80; IDLE()
+#define BIT1() PORTD = 0x10; PORTD = 0x10; IDLE()
+#define STOP() PORTD = 0xff; PORTD = 0xff
+#endif
 
 void setup()
 {
-    pinMode(Pin, OUTPUT);
+    pinMode(Bit0, OUTPUT);
+    pinMode(Bit1, OUTPUT);
+    pinMode(Frame, OUTPUT);
+    STOP();
 }
 
 void loop()
 {
     while (true)
     {
-    digitalWrite(Pin, LOW);
-    delayMicroseconds(100);
-    digitalWrite(Pin, HIGH);
-    digitalWrite(Pin, LOW);
-    digitalWrite(Pin, HIGH);
+        STOP();
+        STOP();
+        STOP();
+        STOP();
+        STOP();
+        STOP();
+        STOP();
+        STOP();
+        STOP();
+        IDLE();
+        BIT0();
+        BIT0();
+        BIT1();
+        BIT1();
+        BIT0();
+        BIT1();
+        BIT1();
+        BIT0();
+        STOP();
     }
 }
